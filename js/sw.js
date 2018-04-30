a96162fbb7eb334b3821d938e2b69d7669066961
MissMonacoin <>
2018-04-30 13:45:25 +0900

    PWA
    Signed-off-by: MissMonacoin <>

--- js/sw.js
+++ js/sw.js
@@ -1,59 +1,27 @@
 let ver= "<!--t:Timestamp-->"
-let cacheData = "<!--t:Caches-->".split(",").map(d=>"assets/"+d)
+let cacheData = "<!--t:Caches-->".split(",").map(d=>"/wallet/dist/assets/"+d+"?t="+ver)+"/wallet"
 let cName = "cache-"+ver
 
-cacheData.push("dist.js")
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(cName)
-      .then((cache) => {
-        return cache.addAll(cacheData);
-      })
-  );
-});
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [cName];
-
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+self.addEventListener('install', e => {
+  const timeStamp = Date.now();
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(cacheData)
+          .then(() => self.skipWaiting());
     })
   );
 });
-self.addEventListener('fetch', (event) => {
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        let fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest)
-          .then((response2) => {
-            if (!response2 || response2.status !== 200 || response2.type !== 'basic') {
-              return response2;
-            }
-
-            // 重要：レスポンスを clone する。レスポンスは Stream で
-            // ブラウザ用とキャッシュ用の2回必要。なので clone して
-            // 2つの Stream があるようにする
-            let responseToCache = response2.clone();
-
-            caches.open(cName)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response2;
-          });
-      })
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
