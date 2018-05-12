d3cea506067c432b442b0b9cebdd5f927e94c01a
MissMonacoin <>
2018-05-12 22:47:28 +0900

    ticker server

--- js/currencyList.js
+++ js/currencyList.js
@@ -89,7 +89,7 @@ const defaultCoins=[
     },
     enableSegwit:false,
     price:{
-      url:"https://insight.neetcoin.jp/api/ticker",
+      url:"https://api.crypto-bridge.org/api/v1/ticker",
       json:true,
       jsonPath:'$[?(@.id=="ZNY_BTC")].last',
       fiat:"btc"
@@ -499,8 +499,7 @@ const defaultCoins=[
     bip21: "neetcoin",
     defaultFeeSatPerByte: 50,
     icon: require("../res/coins/neet.png"),
-    apiEndpoints:[{url:"https://insight.neetcoin.jp/api",explorer:"https://insight.neetcoin.jp"},
-                 {url:"https://neet.insight.monacoin.ml/api",explorer:"https://insight.neetcoin.jp"}],
+    apiEndpoints:[{url:"https://neet.insight.monacoin.ml/api",explorer:"https://insight.neetcoin.jp"},{url:"https://insight.neetcoin.jp/api",explorer:"https://insight.neetcoin.jp"}],
     network: {
       messagePrefix: '\x19NEETCOIN Signed Message:\n',
       bip32: {
@@ -514,7 +513,7 @@ const defaultCoins=[
     enableSegwit: false,
     lib: "blk",
     price: {
-      url: "https://insight.neetcoin.jp/api/ticker",
+      url: "https://api.crypto-bridge.org/api/v1/ticker",
       json: true,
       jsonPath: '$[?(@.id=="NEET_BTC")].last',
       fiat: "btc"
