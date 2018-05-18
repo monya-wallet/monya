25661a01bde52742d3e6dd1b2b68a9f7131e85cf
MissMonacoin <yjsnpi@lingobrother.local>
2018-05-18 21:38:41 +0900

    Fixed Extension Sending

--- component/navigator.js
+++ component/navigator.js
@@ -114,8 +114,8 @@ module.exports=require("../js/lang.js")({ja:require("./ja/navigator.html"),en:re
             amount:res.amount,
             label:res.label
           })
-          this.$emit("pop")
-          this.$emit("push",res.extension.component)
+          this.pageStack.push(res.extension.component)
+          
         }else if(res.apiName){
           coinUtil.callAPI(res.apiName,res.apiParam)
         }
