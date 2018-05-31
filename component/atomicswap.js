13550b5b30384c9165a93cad5ed9b54e86253a13
nao20010128nao <nao20010128@gmail.com>
2018-05-31 23:34:27 +0900

    Fixed vout for CLTV mode in AtomicSwap (#32)

--- component/atomicswap.js
+++ component/atomicswap.js
@@ -395,7 +395,7 @@ module.exports=require("../js/lang.js")({ja:require("./ja/atomicswap.html"),en:r
         }else if(this.contractType==="cltv"){
           isRefund&&txb.setLockTime(parseInt(this.lockTime))
           res.utxos.forEach(v=>{
-            const vin =txb.addInput(v.txId, 0,isRefund?0:null)
+            const vin =txb.addInput(v.txId, v.vout,isRefund?0:null)
             txb.inputs[vin].value=v.value
           })
         }else{
