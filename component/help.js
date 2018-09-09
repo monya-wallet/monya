a73d973958d0c6665bcffcacc3ff3e9e690483d6
MissMonacoin <yjsnpi@lingobrother.local>
2018-09-09 19:55:58 +0900

    Removed faucet

--- component/help.js
+++ component/help.js
@@ -30,31 +30,6 @@ module.exports=require("../js/lang.js")({ja:require("./ja/help.html"),en:require
     },
     openLink(url){
        coinUtil.openUrl(url)
-    },
-    mineZeny(){
-      
-    },
-    faucet(){
-      storage.get("question").then(r=>{
-        let question;
-        if(r&&r.length){
-          question=r.reduce((acc,v)=>{
-            if(typeof(v)==="string"){
-              acc+="["+v+"]"
-            }else if(typeof(v)==="number"&&0<=v&&v<=15){
-              acc+=v.toString(16)
-            }else if(typeof(v)==="boolean"){
-              acc+=v?"Y":"N"
-            }else{
-              acc+="X"
-            }
-            return acc
-          },"")
-        }else{
-          question=""
-        }
-        coinUtil.openUrl(`https://faucetparty.herokuapp.com?question=${encodeURIComponent(question)}&isNative=${!!coinUtil.isCordovaNative()}`)
-      })
     }
   },
   mounted(){
