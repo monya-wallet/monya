e3e9223dce9adb5c88c269b0b59bcd9815a3790a
Izuho Fujiwara <contact@fujishan.jp>
2019-01-06 00:58:57 +0900

    Fix Currency.js

--- js/currency.js
+++ js/currency.js
@@ -445,9 +445,11 @@ module.exports=class{
       }else if(this.libName==="btg"){
         txb.enableBitcoinGold(true)
         txb.sign(i,keyPair,null,this.lib.Transaction.SIGHASH_ALL | this.lib.Transaction.SIGHASH_BITCOINCASHBIP143,txb.inputs[i].value)
+      }else if(this.libName==="zec" && this.network.txversion===3){
+        txb.sign(i,keyPair,null,this.lib.Transaction.SIGHASH_ALL,txb.inputs[i].value,null,3)
       }else if(this.libName==="zec" && this.network.txversion===4){
-        txb.sign(i,keyPair,null,this.lib.Transaction.SIGHASH_ALL,txb.inputs[i].value,null,true)
-      }else{
+	    txb.sign(i,keyPair,null,this.lib.Transaction.SIGHASH_ALL,txb.inputs[i].value,null,4)
+	  }else{
         txb.sign(i,keyPair)
       }
     }
